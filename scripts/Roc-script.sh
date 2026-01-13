a287a3025af0caace7157a3134fea1fc11eb30cb
null <68135821+shoujidi@users.noreply.github.com>
2026-01-13 20:09:06 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -58,7 +58,7 @@ git_sparse_clone openwrt-23.05 https://github.com/immortalwrt/packages utils/wat
 mv -f package/watchcat feeds/packages/utils/watchcat
 git_sparse_clone openwrt-23.05 https://github.com/immortalwrt/luci applications/luci-app-watchcat
 mv -f package/luci-app-watchcat feeds/luci/applications/luci-app-watchcat
-# git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-wolplus
+git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-wolplus
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config feeds/luci/applications/luci-app-argon-config
 git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora feeds/luci/themes/luci-theme-aurora
@@ -66,16 +66,15 @@ git clone --depth=1 https://github.com/eamonxg/luci-app-aurora-config feeds/luci
 git clone --depth=1 https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 git clone --depth=1 https://github.com/sbwml/luci-app-openlist2 package/openlist2
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/luci-app-lucky
-# git clone --depth=1 https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
-# git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone --depth=1 https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
+git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
 
 
-# 将默认主题从 Aurora 改为 argone (或其他你已包含的主题)
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
 ### PassWall & OpenClash ###
 
 # 移除 OpenWrt Feeds 自带的核心库
